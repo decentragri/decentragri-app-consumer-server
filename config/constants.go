61f686482979ced1bc612a3a0f6611f4e7535d3c
YirenNing24 <paulanthonyarriola@gmail.com>
2025-08-29 03:31:25 +0800

    feat: Updated const

--- config/constants.go
+++ config/constants.go
@@ -3,13 +3,10 @@ package config
 // Blockchain network configuration
 const (
 	CHAIN = "421614" //Arbitrum Sepolia
-	FarmPlotContractAddress = "0x3a981F929b06a44F739C8832e030bEF3881742dc"
+	FarmPlotContractAddress = "0xF2F3ca589d7d2e7b73Bf6461c5028b4f382f1655"
 	AdminWallet = "0xE37D4e372c004ff76c1415d3C711B7dD1BbCCCeB"
-	DAGRIContractAddress = "0xF21C7E1DC1dB0903C2A4BC015A9825081682D448"
+	DAGRIContractAddress = "0xC16512472E334DbB7D896ee09bBe70fa4464F90E"
 	TreasuryWallet = "0x984785A89BF95cb3d5Df4E45F670081944d8D547"
-	MarketPlaceContractAddress = "0x204239c1A74e6d1D17A6FE52172f3F8D26597DB9"
-
+	MarketPlaceContractAddress = "0x3F32B096D519dd29bdbea154387784c373f89687"
 	EngineCloudBaseURL = "https://engine.decentragri.com"
-
-
 )
